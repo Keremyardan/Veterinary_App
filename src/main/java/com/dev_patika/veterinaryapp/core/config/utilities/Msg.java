dba11b5c7c52d3d8439954d6e1776ffd1f735e73
Kerem Yardan <keremyrdn@hotmail.com>
2024-05-25 00:21:33 +0300

    message translations have been made

--- src/main/java/com/dev_patika/veterinaryapp/core/config/utilities/Msg.java
+++ src/main/java/com/dev_patika/veterinaryapp/core/config/utilities/Msg.java
@@ -1,15 +1,15 @@
 package com.dev_patika.veterinaryapp.core.config.utilities;
 
 public class Msg { // This class contains messages for Result objects.
-    public static final String CREATED = "Veri ekleme işlemi başarılı";
-    public static final String OK = "İşlem başarılı";
-    public static final String VALIDATE_ERROR = "Veri doğrulama hatası";
-    public static final String NOT_FOUND = "Veri bulunamadı";
-    public static final String VACCINE_PROTECTION_DATE_NOT_ARRIVED = "Aynı isim ve kod ile aşı zaten var ve koruma bitiş tarihi henüz gelmedi.";
-    public static final String DOCTOR_NOT_AVAILABLE = "Doktor belirtilen tarih ve saatte müsait değil.";
-    public static final String ANIMAL_NOT_FOUND = "Hayvan bulunamadı";
-    public static final String SAME_EMAIL = "Bu e-posta adresi ile bir kayıt zaten var.";
-    public static final String SAME_PHONE = "Bu telefon numarası ile bir kayıt zaten var.";
-    public static final String SAME_VACCINE_NAME_AND_CODE = "Aynı kod ile aşı zaten var.";
-    public static final String APPOINTMENT_ALREADY_EXISTS = "Bu tarih ve saatte randevu zaten var." ;
+    public static final String CREATED = "Data addition is successful";
+    public static final String OK = "Transaction successful";
+    public static final String VALIDATE_ERROR = "Data validation error";
+    public static final String NOT_FOUND = "Data could not found";
+    public static final String VACCINE_PROTECTION_DATE_NOT_ARRIVED = "There is already a vaccine with the same name and code, and the end date of protection has not yet arrived";
+    public static final String DOCTOR_NOT_AVAILABLE = "The doctor is not available on the specified date and time";
+    public static final String ANIMAL_NOT_FOUND = "Animal not found";
+    public static final String SAME_EMAIL = "A record already exists with this email address";
+    public static final String SAME_PHONE = "A record already exists with this phone number";
+    public static final String SAME_VACCINE_NAME_AND_CODE = "There is already a vaccine with the same code";
+    public static final String APPOINTMENT_ALREADY_EXISTS = "There is already an appointment for this date and time" ;
 }
